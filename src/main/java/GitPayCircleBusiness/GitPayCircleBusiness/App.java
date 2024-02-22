5256693fb42a42dac2be5986d192397f19f40267
Lokesh R B <Lokesh R B@DESKTOP-F6CAB6A>
2024-02-22 15:13:53 +0530

    First commit....

---
+++ src/main/java/GitPayCircleBusiness/GitPayCircleBusiness/App.java
@@ -0,0 +1,13 @@
+package GitPayCircleBusiness.GitPayCircleBusiness;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
